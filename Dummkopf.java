fdf959ff43aa1447fbffc629e9ae9071dbdc6b2d
Fabian Pasch <fabian.pasch.speyer@gmail.com>
2023-11-15 20:23:31 +0100

    Version 1.1 MoveAble

--- Dummkopf.java
+++ Dummkopf.java
@@ -3,9 +3,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
 public class Dummkopf extends Actor
 {
     public void act() {
-        if(Greenfoot.isKeyDown("left"))
+        // Move with WASD-Keys
+        if(Greenfoot.isKeyDown("a"))
             setLocation(getX() - 3, getY());
-        if(Greenfoot.isKeyDown("right"))
+        if(Greenfoot.isKeyDown("d"))
             setLocation(getX() + 3, getY());
+            if(Greenfoot.isKeyDown("w"))
+            setLocation(getX(), getY()-3);
+        if(Greenfoot.isKeyDown("s"))
+            setLocation(getX() , getY()+3);
     }    
 }
