62afc7a1d918f72cf5cb019869936d66f69997ff
Fabian Pasch <fabian.pasch.speyer@gmail.com>
2023-11-29 12:37:43 +0100

    Background

--- MyWorld.java
+++ MyWorld.java
@@ -8,5 +8,13 @@ public class MyWorld extends World
         super(600, 400, 1); 
         Dummkopf dummkopf = new Dummkopf();
         addObject(dummkopf,300,200);
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
